99a6bf87232db91b0c6d5ad2b736734a9350a777
SHAHRIAR UTCHAS <123489314+Shahriar-Utchas@users.noreply.github.com>
2025-05-20 13:51:04 +0600

    basic structure built

---
+++ src/Layouts/Root.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
+
+const Root = () => {
+    return (
+        <>
+            <div className='min-h-screen'>
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+                <Footer></Footer>
+            </div>
+        </>
+    );
+};
+
+export default Root;
